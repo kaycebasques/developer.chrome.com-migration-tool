72e56938cde76d1d63254a65724bc36a68dfe298
Kayce Basques <kayce@google.com>
2020-09-17 10:28:26 -0700

    Add basic migration logic

---
+++ index.js
@@ -0,0 +1,72 @@
+const readline = require('readline');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const TurndownService = require('turndown');
+const outputDirectory = 'output';
+
+function init() {
+  const targets = [];
+  // TODO(kaycebasques): Delete the old output directory?
+  fs.rmdirSync(outputDirectory, {recursive: true});
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream('targets.txt')
+  });
+  readInterface.on('line', line => {
+    targets.push(line);
+  });
+  readInterface.on('close', () => {
+    migrate(targets);
+  });
+}
+
+async function modify(page) {
+  await page.$$eval('pre.prettyprint', targets => {
+    targets.forEach(target => {
+      const code = document.createElement('code');
+      code.innerHTML = target.innerHTML;
+      target.innerHTML = '';
+      target.appendChild(code);
+    });
+  });
+}
+
+async function cleanup(page) {
+  const selectors = [
+    'nav.inline-toc',
+    'a.permalink'
+  ];
+  for (let i = 0; i < selectors.length; i++) {
+    const selector = selectors[i];
+    await page.$$eval(selector, nodes => nodes.forEach(node => node.remove()));
+  }
+}
+
+async function migrate(targets) {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+  for (let i = 0; i < 3; i++) {
+    const target = targets[i];
+    const pathname = new URL(target).pathname;
+    const destination = `${outputDirectory}${pathname}`;
+    fs.mkdirSync(destination, {recursive: true});
+    await page.goto(target, {
+      waitUntil: 'networkidle2'
+    });
+    await cleanup(page);
+    await modify(page);
+    const html = await page.$eval('div[itemprop="articleBody"]', element => element.innerHTML);
+    const turndownService = TurndownService({
+      headingStyle: 'atx',
+      codeBlockStyle: 'fenced',
+      linkStyle: 'referenced'
+    });
+    const markdown = turndownService.turndown(html);
+    fs.writeFileSync(`${destination}/index.md`, markdown);
+  }
+  await browser.close();
+}
+
+//start();
+init();
